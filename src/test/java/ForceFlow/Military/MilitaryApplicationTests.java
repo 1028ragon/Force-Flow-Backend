31744edee7c8432d4cca425f436dea6188df0526
이민구 <minet0401@gmail.com>
2026-06-23 12:38:06 +0900

    init: 초기setting

---
+++ src/test/java/ForceFlow/Military/MilitaryApplicationTests.java
@@ -0,0 +1,13 @@
+package ForceFlow.Military;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MilitaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
